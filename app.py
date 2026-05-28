d0c9d23813e571e5ef5331fbb5fee0b7cea28b42
thyezoliveira <thyezoliveira@gmail.com>
2026-05-28 13:45:38 -0300

    feat(app): Adição de CORS

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from pprint import pprint
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/inscricao_entrada", methods=["POST"])
 def inscricao_entrada():
